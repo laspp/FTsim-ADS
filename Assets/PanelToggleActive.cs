c9381047255423f7aec5b258e36861686074b839
Nejc Ilc <nejc.ilc@gmail.com>
2023-10-03 13:03:09 +0200

    UI - grid layouts

---
+++ Assets/PanelToggleActive.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelToggleActive : MonoBehaviour
+{
+    
+    public void ToggleVisibility()
+    {
+        if (gameObject.activeInHierarchy)
+            gameObject.SetActive(false);
+        else
+            gameObject.SetActive(true);
+    }
+}
