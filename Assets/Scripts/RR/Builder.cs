1b9f6e1c6e3746435b84629a0e4bb5c345157967
Nejc Ilc <nejc.ilc@gmail.com>
2024-02-06 12:43:16 +0100

    SmartBar core functionality (scale gradually)

--- Assets/Scripts/RR/Builder.cs
+++ Assets/Scripts/RR/Builder.cs
@@ -8,21 +8,29 @@ public class Builder : MonoBehaviour
 
     public GameObject buttonReset;
     public GameObject buttonSave;
+    public GameObject toggleSpawn;
 
     Vector3 initScale = new Vector3(1,1,1);
     Button btnSave, btnReset;
+    Toggle tglSpawn;
 
     bool builderMode;
     public bool BuilderMode { get => builderMode; set => builderMode = value; }
+
+    bool spawnMode;
+    public bool SpawnMode { get => spawnMode; set => spawnMode = value; }
 
     // Start is called before the first frame update
     void Awake()
     {
         BuilderMode = false;
+        SpawnMode = false;
         btnSave = buttonSave.GetComponent<Button>();
         btnReset = buttonReset.GetComponent<Button>();
+        tglSpawn = toggleSpawn.GetComponent<Toggle>();
         btnSave.interactable = false;
         btnReset.interactable = false;
+        tglSpawn.interactable = false;
     }
 
     // Update is called once per frame
@@ -44,18 +52,24 @@ public class Builder : MonoBehaviour
         }
     }
 
-    public void ToggleButtonOnChange()
+    public void ToggleModeOnChange()
     {
         BuilderMode = !BuilderMode;
         if (BuilderMode)
         {
             btnSave.interactable = true;
             btnReset.interactable = true;
+            tglSpawn.interactable = true;
         }
         else
         {
             btnSave.interactable = false;
             btnReset.interactable = false;
+            tglSpawn.interactable = false;
         }
+    }
+    public void ToggleSpawnOnChange()
+    {
+        SpawnMode = !SpawnMode;
     }
 }
--- Assets/Scripts/RR/SmartBar.cs
+++ Assets/Scripts/RR/SmartBar.cs
@@ -5,39 +5,73 @@ using UnityEngine.EventSystems;
 
 public class SmartBar : MonoBehaviour, IPointerClickHandler
 {
-    [Tooltip("Amount of growing/shrinking when clicked.")]
-    public float growBy = 1.0f;
-    public float maxHeight = 5.0f;
+    [Tooltip("Amount of growing/shrinking when clicked. 1 means the height of a workpiece.")]
+    public float scaleStep = 1.0f;
+    public float scaleSpeed = 1.0f;
+    public float maxScale = 5.0f;
     public GameObject panelBuilder;
+    public Transform workpieceSpawnPoint;
 
     MeshRenderer objMeshRederer;
     Color origColor;
     Vector3 growVector;
-    float origHeight;
+    float origScale;
+    float currentScale;
+    float targetScale;
+    float scaleEps = 0.01f;
     Builder builderScript;
 
 
     void Awake()
     {
-        origHeight = transform.parent.localScale.y;
+        origScale = transform.parent.localScale.y;
+        currentScale = origScale;
+        targetScale = currentScale;
         objMeshRederer = GetComponent<MeshRenderer>();
         origColor = objMeshRederer.material.color;
-        growVector = new Vector3(0, growBy, 0);
+        growVector = new Vector3(0, scaleStep, 0);
         builderScript = panelBuilder.GetComponent<Builder>();
     }
 
+    void FixedUpdate()
+    {
+        // Get the current scale in the Y direction
+        currentScale = transform.parent.localScale.y;
+        float diff = targetScale - currentScale;
+
+        // Check if the current scale is not approximately equal to the target scale
+        if (Mathf.Abs(diff) > scaleEps)
+        {
+            // Determine whether to increase or decrease the scale based on the difference between current and target scales
+            float scaleChange = Mathf.Sign(diff) * scaleSpeed * Time.fixedDeltaTime;
+
+            // Apply the scale change
+            transform.parent.localScale += new Vector3(0f, scaleChange, 0f);
+        }
+    }
+
+
+
     void OnMouseEnter()
     {
-        if (builderScript.BuilderMode) {
-            objMeshRederer.material.color = Color.green;
+        if (builderScript.BuilderMode)
+        {
+            if (builderScript.SpawnMode)
+            {
+                objMeshRederer.material.color = Color.yellow;
+            }
+            else
+            {
+                objMeshRederer.material.color = Color.green;
+            }
         }
     }
     void OnMouseExit()
     {
-        if (builderScript.BuilderMode)
-        {
-            objMeshRederer.material.color = origColor;
-        }
+        //if (builderScript.BuilderMode)
+        //{
+        objMeshRederer.material.color = origColor;
+        //}
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -46,20 +80,30 @@ public class SmartBar : MonoBehaviour, IPointerClickHandler
         {
             if (eventData.button == PointerEventData.InputButton.Left)
             {
-                // Let's scale the cube up by growBy
-                // To scale in only one direction, we actually scale its parent (empty gameObject placed at the base of a bar)
-                if (transform.parent.localScale.y < maxHeight)
+                if (builderScript.SpawnMode)
                 {
-                    transform.parent.localScale += growVector;
+                    // Set spawn point above this bar
+                    workpieceSpawnPoint.position = new Vector3(transform.position.x, maxScale, transform.position.z);
+                }
+                else
+                {
+                    // Let's scale the cube up by growBy
+                    // To scale in only one direction, we actually scale its parent (empty gameObject placed at the base of a bar)
+                    if (transform.parent.localScale.y < maxScale)
+                    {
+                        //transform.parent.localScale += growVector;
+                        targetScale += scaleStep;
+                    }
                 }
 
             }
             if (eventData.button == PointerEventData.InputButton.Right)
             {
                 // Let's scale the cube down by growBy
-                if (transform.parent.localScale.y > origHeight)
+                if (transform.parent.localScale.y > origScale)
                 {
-                    transform.parent.localScale -= growVector;
+                    //transform.parent.localScale -= growVector;
+                    targetScale -= scaleStep;
                 }
             }
         }
