22e6303f16aad0e78eb4c9c253098bca8d1d3fd4
Nejc Ilc <nejc.ilc@gmail.com>
2024-03-01 14:38:19 +0100

    Add status bar UI draft, polish FPS text

--- Assets/Scripts/ShowFPS.cs
+++ Assets/Scripts/ShowFPS.cs
@@ -1,44 +1,47 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using TMPro;
+using UnityEngine;
 
-public class ShowFPS : MonoBehaviour {
+public class ShowFPS : MonoBehaviour
+{
+    public GameObject communication;
+    public TextMeshProUGUI fpsText;
 
-	public Text fpsText;
+    Communication comScript;
+    private int frameCount;
+    private double dt;
+    private int fps;
+    private double updateRate;
+    private bool showFPS = false;
 
-	private Communication com;
-	private int frameCount;
-	private double dt;
-	private int fps;
-	private double updateRate;
-	private bool showFPS = false;
+    // Use this for initialization
+    void Start()
+    {
+        frameCount = 0;
+        dt = 0.0;
+        fps = 0;
+        updateRate = 4.0;  // 4 updates per sec.
 
-	// Use this for initialization
-	void Start () {
-		frameCount = 0;
-		dt = 0.0;
-		fps = 0;
-		updateRate = 4.0;  // 4 updates per sec.
+        comScript = communication.GetComponent<Communication>();
+        showFPS = comScript.appConfig.ShowFPS;
+        fpsText.text = "";
+    }
 
-		com = GameObject.Find("Communication").GetComponent<Communication>();
-		showFPS = com.appConfig.ShowFPS;
-		fpsText.text = "";
-	}
-	
-	void Update () {
+    void Update()
+    {
         if (showFPS)
         {
-			frameCount++;
-			dt += Time.deltaTime;
-			if (dt > 1.0 / updateRate)
-			{
-				fps = Mathf.FloorToInt((float)(frameCount / dt));
-				frameCount = 0;
-				dt -= 1.0 / updateRate;
-				fpsText.text = "FPS: " + fps.ToString();
-			}
-		}
-		
-	}
+            frameCount++;
+            dt += Time.deltaTime;
+            if (dt > 1.0 / updateRate)
+            {
+                fps = Mathf.FloorToInt((float)(frameCount / dt));
+                frameCount = 0;
+                dt -= 1.0 / updateRate;
+                fpsText.text = "FPS: " + fps.ToString();
+            }
+        }
+
+    }
 }
 
 
